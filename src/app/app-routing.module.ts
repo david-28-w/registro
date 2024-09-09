9a4a2dede11bf4ecf1f078a716309d5861531ab6
david-28-w <david.garzonl@cun.edu.co>
2024-09-08 22:48:07 -0500

    registrador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { ProductoComponent } from './producto/producto.component';
+import { RegistroProductoComponent } from './registro-producto/registro-producto.component';
+import { RegistroUsuariosComponent } from './registro-usuarios/registro-usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path: 'usuarios', component:UsuarioComponent},
+
+{path: 'productos', component:ProductoComponent},
+
+{path: 'registroproducto', component:RegistroProductoComponent},
+
+{path: 'registrousuario', component:RegistroUsuariosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './producto/producto.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { RegistroUsuariosComponent } from './registro-usuarios/registro-usuarios.component';
+import { RegistroProductoComponent } from './registro-producto/registro-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoComponent,
+    UsuarioComponent,
+   RegistroProductoComponent,
+RegistroUsuariosComponent
   ],
   imports: [
     BrowserModule,
